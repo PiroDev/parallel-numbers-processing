8cda56b5e8f4dd09de4c6953e481931c412d7a2d
Muravev Ilia <degtyarev327@gmail.com>
2020-10-28 17:32:51 +0300

    add script for unit and stress testing

---
+++ unit_and_stress_tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+mkdir -p out
+
+echo "RUN unit testing for serial library build with valgrind"
+valgrind --leak-check=full ./build/unit_tests_serial
+gcovr build/CMakeFiles/serial.dir/project/lib/serial -o out/serial_coverage_report.txt
+echo
+cat out/serial_coverage_report.txt
+echo
+
+echo "RUN unit testing for multithread library build with valgrind"
+valgrind --leak-check=full ./build/unit_tests_parallel
+gcovr build/CMakeFiles/parallel.dir/project/lib/parallel -o out/parallel_coverage_report.txt
+echo
+cat out/parallel_coverage_report.txt
+echo
+
+echo "RUN stress testing for multithred and serial libraries (result compare)"
+cd build
+export LD_LIBRARY_PATH=`pwd`
+./stress_tests
