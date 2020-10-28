370d252399daa8b7c63e539143693d5407aa1f3f
Muravev Ilia <degtyarev327@gmail.com>
2020-10-28 19:21:44 +0300

    fix gitlab-ci and improve script for unit testing

--- unit_and_stress_tests.sh
+++ unit_and_stress_tests.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail
+
+function print_header() {
+    echo -e "\n***** ${1} *****"
+}
+
 mkdir -p out
 
-echo "RUN unit testing for serial library build with valgrind"
+print_header "RUN unit testing for serial library build with valgrind"
 valgrind --leak-check=full ./build/unit_tests_serial
 gcovr build/CMakeFiles/serial.dir/project/lib/serial -o out/serial_coverage_report.txt
 echo
 cat out/serial_coverage_report.txt
 echo
 
-echo "RUN unit testing for multithread library build with valgrind"
+print_header "RUN unit testing for multithread library build with valgrind"
 valgrind --leak-check=full ./build/unit_tests_parallel
 gcovr build/CMakeFiles/parallel.dir/project/lib/parallel -o out/parallel_coverage_report.txt
 echo
 cat out/parallel_coverage_report.txt
 echo
 
-echo "RUN stress testing for multithred and serial libraries (result compare)"
+print_header "RUN stress testing for multithred and serial libraries (result compare)"
 cd build
 export LD_LIBRARY_PATH=`pwd`
 ./stress_tests
